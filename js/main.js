f0984d51cf2ad519f148c9c61a458fd2e42e088f
HarrisonCreates <harrison.burr@uconn.edu>
2018-09-06 16:35:39 -0400

    added tracking to browser, needs tweaking

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-var video = document.querySelector("#webcamFeed");
+window.onload = function() {
+  var video = document.getElementById('webcamFeed');
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
 
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({
-      video: true
-    })
-    .then(function(stream) {
-      video.srcObject = stream;
-    })
-    .catch(function(err0r) {
-      console.log("Please make sure a webcam is connected. If there is one connected, make sure it's the right one.");
+  var tracker = new tracking.ObjectTracker('face');
+  tracker.setInitialScale(4);
+  tracker.setStepSize(2);
+  tracker.setEdgesDensity(0.1);
+
+  tracking.track('#webcamFeed', tracker, { camera: true });
+
+  tracker.on('track', function(event) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    event.data.forEach(function(rect) {
+      context.strokeStyle = '#a64ceb';
+      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context.font = '11px Helvetica';
+      context.fillStyle = "#fff";
+      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
     });
-}
+  });
+
+  var gui = new dat.GUI();
+  gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
+  gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
+  gui.add(tracker, 'stepSize', 1, 5).step(0.1);
+};
